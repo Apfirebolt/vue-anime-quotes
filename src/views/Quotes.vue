43970c2c36fcd7a81db84c05ea093e30493d31e4
Apfirebolt <aspper20@gmail.com>
2025-03-21 23:00:19 +0530

    added search fields

--- src/views/Quotes.vue
+++ src/views/Quotes.vue
@@ -1,17 +1,23 @@
 <template>
     <div class="home">
         <h1>Welcome to Anime Quotes</h1>
-        <p>Get inspired by random quotes from your favorite anime!</p>
+        <p>Get inspired by random quotes from your favorite anime! {{ showItems }}</p>
         <div class="quotes-container"></div>
         <div v-if="isLoading">
             <Loader />
         </div>
         <div v-else class="quotes-grid">
+            <v-text-field
+                v-model="searchQuery"
+                label="Search Quotes"
+                class="mb-4"
+                clearable
+            ></v-text-field>
             <v-row dense>
-                <v-col v-for="(quote) in quotes.slice(0, showItems)" :key="quote._id" cols="12" md="4">
+                <v-col v-for="(quote) in filteredQuotes.slice(0, showItems)" :key="quote._id" cols="12" md="4">
                     <v-card color="error" dark class="mb-4">
-                        <v-card-title>{{ quote.show }}</v-card-title>
-                        <v-card-subtitle>{{ quote.character }}</v-card-subtitle>
+                        <v-card-title style="font-size: 1.5rem;">{{ quote.show }}</v-card-title>
+                        <v-card-subtitle style="font-size: 1.2rem;">{{ quote.character }}</v-card-subtitle>
                         <v-card-text>{{ quote.quote }}</v-card-text>
                     </v-card>
                 </v-col>
@@ -21,9 +27,9 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import Loader from "../components/Loader.vue";
-import { VRow, VCol, VCard, VCardTitle, VCardSubtitle, VCardText } from 'vuetify/components';
+import { VRow, VCol, VCard, VCardTitle, VCardSubtitle, VCardText, VTextField } from 'vuetify/components';
 
 interface Quote {
     _id: string;
@@ -36,6 +42,14 @@ const quotes = ref<Quote[]>([]);
 const isLoading = ref<boolean>(false);
 const showItems = ref<number>(40);
 const isScrolling = ref<boolean>(false);
+const searchQuery = ref<string>('');
+
+const filteredQuotes = computed(() => {
+    return quotes.value.filter((quote) => {
+        const searchLower = searchQuery.value.toLowerCase();
+        return quote.show.toLowerCase().includes(searchLower) || quote.character.toLowerCase().includes(searchLower);
+    });
+});
 
 const handleScroll = () => {
     if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isScrolling.value) return;
